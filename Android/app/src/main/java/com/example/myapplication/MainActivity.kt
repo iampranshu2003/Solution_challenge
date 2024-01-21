a051dd8fcdb4e065e795dcb132bdededd3f1dc28
Pranshu <iampranshu2003>
2024-01-21 10:00:39 +0530

    Added my Android app

---
+++ Android/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var getStartedButton: Button
+    private lateinit var loginButton: Button
+    private lateinit var registerTv : TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        getStartedButton = findViewById(R.id.getStartedBtn)
+        loginButton = findViewById(R.id.loginBtn)
+        registerTv = findViewById(R.id.signUpTv)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+        registerTv.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
---
+++ Android/app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val email = findViewById<EditText>(R.id.et_email).text.toString()
+        val Password = findViewById<EditText>(R.id.editTextTextPassword).text.toString()
+
+
+        if (email.isNotEmpty() && Password.isNotEmpty())  {
+
+                firebaseAuth.createUserWithEmailAndPassword(email, Password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            }
+         else {
+            Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+}
